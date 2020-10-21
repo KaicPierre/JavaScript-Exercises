1d144de6c25c54b9fe8b80ebdf34a8c10b3a1aa6
Kaic Pierre Silva <kpisilva@haoc.com.br>
2020-10-21 14:56:13 -0300

    Atualizações dos ex - Fazendo calculadora
    Começo da Calculadora sem usar eval( ).

--- binaryToDecimal/binaryToDecimal.js
+++ binaryToDecimal/binaryToDecimal.js
@@ -1,11 +1,19 @@
+function binaryToDecimal() {
+    let binaryNumber = document.getElementById("binaryNumber").value;
 
-function binaryToDecimal(){
-    let bin = document.getElementById('bin').value;
-
-    console.log(bin)
-    let sum = 0
-    for (let i = 0; i < bin.length; i++){
-        sum += Math.pow(2, parseInt(bin) -i -1)
+    for (let index = 0; index < binaryNumber.length; index++) {
+        if (binaryNumber[index] != "0" && binaryNumber[index] != "1") {
+            alert("Only 0 and 1 at the Binary Input");
+            document.getElementById("binaryNumber").value = ""
+            return 0;
+        }
     }
-    document.getElementById('textview').value = sum
+
+    let integerNumber = 0;
+
+    for (let index = 0; index < binaryNumber.length; index++) {
+        let bit = binaryNumber.charAt(binaryNumber.length - index - 1);
+         integerNumber += parseInt(bit) * Math.pow(2, index);
+    }
+    document.getElementById("textview").value = integerNumber;
 }
